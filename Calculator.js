ff32d5215576b33e78925a1bd481933727c78773
Rohit Santram Tawade <115890889+RohitTawade@users.noreply.github.com>
2023-08-09 22:25:29 +0530

    Create Calculator.js

---
+++ Calculator.js
@@ -0,0 +1,9 @@
+class Calculator {
+    static add(a, b) {
+        return a + b;
+    }
+}
+
+// Call the static add method of the Calculator class
+const result = Calculator.add(10, 5);
+console.log(result); // Output: 15
